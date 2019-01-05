4fb25e357c09f35c4beefb8076a235e028caac32
jackalstar88 <jackalstar88@gmail.com>
2019-01-05 14:31:37 -0200

    change route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended:true }));
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (_req, res) => {
+  app.get('/', (_req, res) => {
       res.send(path.join(__dirname, 'client/build', 'index.html'));
   })
 
