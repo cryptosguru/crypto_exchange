6f5590f58630ded464d1a09f68916985fc28f104
jackalstar88 <jackalstar88@gmail.com>
2019-01-05 14:27:32 -0200

    adding request to root

--- client/src/App.js
+++ client/src/App.js
@@ -32,6 +32,23 @@ class App extends Component {
     activeRoute: `/`
   }
 
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+  this.callBackendAPI()
+    .then(res => this.setState({ data: res.express }))
+    .catch(err => console.log(err));
+}
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+callBackendAPI = async () => {
+  const response = await fetch('/test');
+  const body = await response.json();
+
+  if (response.status !== 200) {
+    throw Error(body.message) 
+  }
+  return body;
+};
+
   render() {
     return (
       <div className="App">
