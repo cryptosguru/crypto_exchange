5ab92c80c66244e5a83c81a1a5c89728e763be29
jackalstar88 <jackalstar88@gmail.com>
2019-01-10 22:46:28 -0200

    change send to sendFile

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('/', (_req, res) => {
-      res.send(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   })    
 
 }  
