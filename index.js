792e1e51c7fec67901317ba05bec6bda7434b4d2
jackalstar88 <jackalstar88@gmail.com>
2019-01-10 23:03:52 -0200

    fix relative path issue~

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(bodyParser.urlencoded({ extended:true }));
 app.use(apiTokenValidator);
 
 if (process.env.NODE_ENV == 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('/', (_req, res) => {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   })    
 
 }  
