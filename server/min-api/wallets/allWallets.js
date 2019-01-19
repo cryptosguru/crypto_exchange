73abf6e189334dd256222524325c336ce2f08698
jackalstar88 <jackalstar88@gmail.com>
2019-01-19 11:06:37 -0200

    changing from security anonymity

--- server/min-api/wallets/allWallets.js
+++ server/min-api/wallets/allWallets.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { compose } from 'ramda';
 import { GENERAL_WALLETS, MIN_API_URL, CRYPTOCOMPARE_WEBSITE } from '../../config';
 
 const walletsFilters = {
   coinsIncludes: coin => ({ coins }) => coins.map(supportedCoin => supportedCoin.toLowerCase()).includes(coin.toLowerCase()),
-  securityIs: walletSecurity => ({ security }) => security === walletSecurity
+  anonymityIs: walletAnonymity => ({ anonymity }) => anonymity === walletAnonymity
 }
 
 /**
@@ -15,14 +14,14 @@ const walletsFilters = {
  * @description Supports two filters, coinIncludes and security.
  */
 const allWallets = async (req, res) => {
-  const { coinsIncludes, security } = req.query;
+  const { coinsIncludes, anonymity } = req.query;
 
   const { data: { Data: walletsWithKeyValue }} = await axios.get(`${MIN_API_URL}${GENERAL_WALLETS}?api_key=${process.env.MIN_API_TOKEN}`)
   
   /**
    * The Goal here was not performance, this could be reduced to just one loop.
    */
-  const securityFilter = security ? walletsFilters.securityIs(security) : () => true;
+  const securityFilter = anonymity ? walletsFilters.anonymityIs(anonymity) : () => true;
   const coinsFilter = coinsIncludes ? walletsFilters.coinsIncludes(coinsIncludes) : () => true;
 
   const allWallets = Object.entries(walletsWithKeyValue)
